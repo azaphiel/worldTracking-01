d6a51797f83a4bc34bdbead810197cc114e94c39
Özkan Adar <ozkanadar@outlook.com>
2018-09-26 13:13:11 +0300

    Initial Commit

---
+++ worldTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  worldTracking
+//
+//  Created by Özkan Adar on 26.09.2018.
+//  Copyright © 2018 Özkan Adar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
